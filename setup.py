92dfecb59178ef0e4d4dd909513a110163571c51
zhixian <zhixian@users.noreply.github.com>
2026-05-26 15:27:53 +0900

    chore: bump to v0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="owlia-nest",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     package_data={"owlia_nest": ["icons/*"]},
